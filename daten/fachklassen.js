e6cfd4317d520e96ccb39b0b34a29b5bf0b8f69e
hmt <dev@hmt.im>
2019-11-26 17:36:35 +0100

    wip

---
+++ daten/fachklassen.js
@@ -0,0 +1,11 @@
+const fkl = {
+  default: {
+    Schulform: 'Schulform fehlt',
+    Bereich: 'Bereich fehlt',
+    Zeugniskopf: 'ohne Angabe',
+    Berufsbezeichnung_m: 'männliche Berufsbezeichnung',
+    Berufsbezeichnung_w: 'weibliche Berufsbezeichnung'
+  }
+}
+
+export default fkl
